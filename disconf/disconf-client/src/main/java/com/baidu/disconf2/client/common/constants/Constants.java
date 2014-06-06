0708eabf71a148ce77b13778a9a07bc5437cadf6
x240pc <knightliao@gmail.com>
2014-06-06 18:08:39 +0800

    re

---
+++ disconf/disconf-client/src/main/java/com/baidu/disconf2/client/common/constants/Constants.java
@@ -0,0 +1,14 @@
+package com.baidu.disconf2.client.common.constants;
+
+/**
+ * 
+ * @author liaoqiqi
+ * @version 2014-6-6
+ */
+public class Constants {
+
+    /**
+     * 
+     */
+    public static final String DISCONF_PACK_NAME = "com.baidu.disconf2";
+}
