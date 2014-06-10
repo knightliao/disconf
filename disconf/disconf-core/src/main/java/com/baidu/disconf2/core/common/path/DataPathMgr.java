e908685c23ee42df83b2185dfd3058bfc48bdaf1
x240pc <knightliao@gmail.com>
2014-06-10 11:45:59 +0800

    路径的设置

---
+++ disconf/disconf-core/src/main/java/com/baidu/disconf2/core/common/path/DataPathMgr.java
@@ -0,0 +1,12 @@
+package com.baidu.disconf2.core.common.path;
+
+/**
+ * 
+ * 获取数据的PATH路径的计算
+ * 
+ * @author liaoqiqi
+ * @version 2014-6-10
+ */
+public class DataPathMgr {
+
+}
---
+++ disconf/disconf-core/src/main/java/com/baidu/disconf2/core/common/path/ZooPathMgr.java
@@ -0,0 +1,11 @@
+package com.baidu.disconf2.core.common.path;
+
+/**
+ * ZOOKEEPER上路径的计算
+ * 
+ * @author liaoqiqi
+ * @version 2014-6-10
+ */
+public class ZooPathMgr {
+
+}
