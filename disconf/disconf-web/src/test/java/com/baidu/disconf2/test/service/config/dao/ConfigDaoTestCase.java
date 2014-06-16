ac36e98563af035950227095d9fa2f2fdf26ac3a
x240pc <knightliao@gmail.com>
2014-06-17 00:38:28 +0800

    添加单测

---
+++ disconf/disconf-web/src/test/java/com/baidu/disconf2/test/service/config/dao/ConfigDaoTestCase.java
@@ -0,0 +1,17 @@
+package com.baidu.disconf2.test.service.config.dao;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.baidu.disconf2.test.common.BaseTestCase;
+
+/**
+ * 
+ * @author liaoqiqi
+ * @version 2014-6-17
+ */
+public class ConfigDaoTestCase extends BaseTestCase {
+
+    protected static final Logger LOG = LoggerFactory
+            .getLogger(ConfigDaoTestCase.class);
+}
--- disconf/disconf-web/src/test/resources/sql/base/schema/1_0_0/dsp.sql
+++ disconf/disconf-web/src/test/resources/sql/base/schema/1_0_0/dsp.sql
@@ -1,6 +1,7 @@
-CREATE DATABASE IF NOT EXISTS `disconf`;
-USE `disconf`;
 
+CREATE SCHEMA IF NOT EXISTS "disconf" /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+DROP TABLE  IF EXISTS disconf."app";
 CREATE TABLE `app` (
     `app_id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '唯一的ID（没有啥意义，主键，自增长而已）',
     `name` VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'APP名(一般是产品线+服务名)',
@@ -8,13 +9,10 @@ CREATE TABLE `app` (
     `create_time` VARCHAR(14) NOT NULL DEFAULT '99991231235959' COMMENT '生成时间',
     `update_time` VARCHAR(14) NOT NULL DEFAULT '99991231235959' COMMENT '修改时',
     PRIMARY KEY (`app_id`)
-)
-COMMENT='app'
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB;
+);
 
 
-
+DROP TABLE  IF EXISTS disconf."config";
 CREATE TABLE `config` (
     `config_id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '唯一的ID（没有啥意义，主键，自增长而已）',
     `type` TINYINT(4) NOT NULL DEFAULT '0' COMMENT '配置文件/配置项',
@@ -26,19 +24,14 @@ CREATE TABLE `config` (
     `create_time` VARCHAR(14) NOT NULL DEFAULT '99991231235959' COMMENT '生成时间',
     `update_time` VARCHAR(14) NOT NULL DEFAULT '99991231235959' COMMENT '修改时间',
     PRIMARY KEY (`config_id`)
-)
-COMMENT='配置'
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB;
+);
 
 
-
+DROP TABLE  IF EXISTS disconf."env";
 CREATE TABLE `env` (
     `env_id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '环境ID（主键，自增长）',
     `name` VARCHAR(255) NOT NULL DEFAULT 'DEFAULT_ENV' COMMENT '环境名字',
     PRIMARY KEY (`env_id`)
-)
-COMMENT='rd/qa/local可以自定义，默认为 DEFAULT_ENV'
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB;
+);
 
+        
