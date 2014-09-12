e4c37148f479c836d4f29c5caec0c04bb1861d1a
x240pc <knightliao@gmail.com>
2014-09-12 14:02:29 +0800

    操作图标化

--- disconf-web/html/assets/js/mainList.js
+++ disconf-web/html/assets/js/mainList.js
@@ -177,16 +177,16 @@
 
 			var link = "";
 			del_link = '<a id="itemDel' + item.configId
-					+ '" style="cursor: pointer; cursor: hand; " >删除</a>';
+					+ '" style="cursor: pointer; cursor: hand; " ><i class="icon-remove"></i></a>';
 			if (item.type == "配置文件") {
 				link = '<a href="modifyFile.html?configId=' + item.configId
-						+ '">修改</a>';
+						+ '"><i class="icon-edit"></i></a>';
 			} else {
 				link = '<a href="modifyItem.html?configId=' + item.configId
-						+ '">修改</a>';
+						+ '"><i class="icon-edit"></i></a>';
 			}
 			var downloadlink = '<a href="/api/config/download/'
-					+ +item.configId + '">下载</a>';
+					+ +item.configId + '"><i class="icon-download-alt"></i></a>';
 
 			return Util.string.format(mainTpl, item.appName, item.appId,
 					item.version, item.envId, item.envName, item.type,
