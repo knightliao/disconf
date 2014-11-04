4b4dc7ad77f84516a1bca7ccc24955b12a89a0ff
knightliao <knightliao@gmail.com>
2014-11-04 16:40:37 +0800

    add test case

--- disconf-core/src/test/java/com/baidu/disconf/core/test/path/DisconfWebPathMgrTestCase.java
+++ disconf-core/src/test/java/com/baidu/disconf/core/test/path/DisconfWebPathMgrTestCase.java
@@ -1,5 +1,10 @@
 package com.baidu.disconf.core.test.path;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.baidu.disconf.core.common.constants.DisConfigTypeEnum;
+import com.baidu.disconf.core.common.path.DisconfWebPathMgr;
 import com.baidu.disconf.core.test.common.BaseCoreTestCase;
 
 /**
@@ -9,4 +14,33 @@ import com.baidu.disconf.core.test.common.BaseCoreTestCase;
  */
 public class DisconfWebPathMgrTestCase extends BaseCoreTestCase {
 
+    @Test
+    public void getZooPrefixUrlTest() {
+
+        String result = DisconfWebPathMgr.getZooPrefixUrl("/test");
+
+        Assert.assertEquals("/test/prefix", result);
+    }
+
+    @Test
+    public void getZooHostsUrlTest() {
+
+        String result = DisconfWebPathMgr.getZooHostsUrl("/test");
+
+        Assert.assertEquals("/test/hosts", result);
+    }
+
+    @Test
+    public void getRemoteUrlParameterTest() {
+
+        String fileUrl =
+                DisconfWebPathMgr.getRemoteUrlParameter("test", "app", "version", "env", "key", DisConfigTypeEnum.FILE);
+        System.out.println(fileUrl);
+        Assert.assertEquals("test/file?app=app&env=env&type=0&key=key&version=version", fileUrl);
+
+        String itemUrl =
+                DisconfWebPathMgr.getRemoteUrlParameter("test", "app", "version", "env", "key", DisConfigTypeEnum.ITEM);
+        System.out.println(fileUrl);
+        Assert.assertEquals("test/item?app=app&env=env&type=1&key=key&version=version", itemUrl);
+    }
 }
