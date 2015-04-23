609ab08fcc45119aaf740fecc66cb733c9c12e97
knightliao <knightliao@gmail.com>
2015-04-23 16:26:29 +0800

    re

--- disconf-web/sql/20150420/disconf-online.sql
+++
@@ -1,7 +0,0 @@
-use `disconf`;
-
-update `app`
-    set `emails`='liaoqiqi@baidu.com;liaoqiqi@baidu.com;xiaoxingxing01@baidu.com;nieyunfei@baidu.com;zhengbingwen@baidu.com;gexiao@baidu.com' where app_id = 4;
-
-/* mobiledsp	mobiledsp_Mhx*/
-INSERT INTO `user` (`user_id`, `name`, `password`, `token`, `ownapps`,`role_id`) VALUES (null, 'mobiledsp', '0855b44a368e44dc6e6825532073b29a368584af', '132069654193f802203d1c6c86e753ecede698f6','4', '1');
