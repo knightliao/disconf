23bcba265502d58123d739d4a818faaee77c1414
knightliao <knightliao@gmail.com>
2016-05-31 10:39:18 +0800

    空的话就不打印了

--- disconf-client/src/main/java/com/baidu/disconf/client/DisconfMgr.java
+++ disconf-client/src/main/java/com/baidu/disconf/client/DisconfMgr.java
@@ -2,6 +2,7 @@ package com.baidu.disconf.client;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
@@ -151,11 +152,18 @@ public class DisconfMgr implements ApplicationContextAware {
         if (DisClientConfig.getInstance().ENABLE_DISCONF) {
 
             //
-            LOGGER.info("Conf File Map: {}", DisconfStoreProcessorFactory.getDisconfStoreFileProcessor()
-                    .confToString());
+            String data = DisconfStoreProcessorFactory.getDisconfStoreFileProcessor()
+                    .confToString();
+            if (!StringUtils.isEmpty(data)) {
+                LOGGER.info("Conf File Map: {}", data);
+            }
+
             //
-            LOGGER.info("Conf Item Map: {}", DisconfStoreProcessorFactory.getDisconfStoreItemProcessor()
-                    .confToString());
+            data = DisconfStoreProcessorFactory.getDisconfStoreItemProcessor()
+                    .confToString();
+            if (!StringUtils.isEmpty(data)) {
+                LOGGER.info("Conf Item Map: {}", data);
+            }
         }
         LOGGER.info("******************************* DISCONF END *******************************");
     }
