143f19412e6a5755418a8007f75c53343e2212e6
Mark Yao <markyao.dev@gmail.com>
2016-08-23 15:35:00 +0800

    不同角色用户显示不用环境env

--- disconf-web/src/main/java/com/baidu/disconf/web/service/role/constant/RoleConstant.java
+++ disconf-web/src/main/java/com/baidu/disconf/web/service/role/constant/RoleConstant.java
@@ -5,11 +5,4 @@ package com.baidu.disconf.web.service.role.constant;
  * @version 2014-1-13
  */
 public class RoleConstant {
-
-    public static final int DEV = 1; //普通开发者
-
-    public static final int MANAGER = 2; //管理员
-
-    public static final int DEV_LEADER = 3; //开发负责人
-
 }
