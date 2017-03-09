8a055f18e61572b9eb24e2e944d21eec13f208aa
yin.shunlin <yin.shunlin@puscene.com>
2017-03-09 15:50:15 +0800

    springboot项目集成： com.paypal.springboot.resteasy-spring-boot-starter的时候， 与disconf发生包冲突，因此将包 org.reflections.reflections的版本从0.9.9-RC1升级到0.9.10

--- disconf-client/src/main/java/com/baidu/disconf/client/scan/inner/statically/strategy/impl/ReflectionScanStatic.java
+++ disconf-client/src/main/java/com/baidu/disconf/client/scan/inner/statically/strategy/impl/ReflectionScanStatic.java
@@ -81,7 +81,7 @@ public class ReflectionScanStatic implements ScanStaticStrategy {
         //
         Collection<URL> urlTotals = new ArrayList<URL>();
         for (String packName : packNameList) {
-            Set<URL> urls = ClasspathHelper.forPackage(packName);
+        	Collection<URL> urls = ClasspathHelper.forPackage(packName);
             urlTotals.addAll(urls);
         }
 
